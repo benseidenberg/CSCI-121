62be8c38a5331b4ae59266810d208d45e978c4c3
Benjamin Seidenberg <44117044+benseidenberg@users.noreply.github.com>
2024-03-29 12:16:17 -0400

    Create BloodData.java

---
+++ BloodGroup/BloodData.java
@@ -0,0 +1,29 @@
+public class BloodData {
+    public enum BloodType {O, A, B, AB}
+
+    private BloodType bloodType;
+    private String rhFactor;
+    public BloodData() {
+        this.bloodType = BloodType.O;
+        this.rhFactor = String.valueOf('+');
+    }
+    public BloodData(BloodType bloodType, String rhFactor) {
+        this.bloodType = bloodType;
+        this.rhFactor = rhFactor;
+    }
+    public BloodType getBloodType() {
+        return bloodType;
+    }
+
+    public void setBloodType(BloodType bloodType) {
+        this.bloodType = bloodType;
+    }
+
+    public String getRhFactor() {
+        return rhFactor;
+    }
+
+    public void setRhFactor(String rhFactor) {
+        this.rhFactor = rhFactor;
+    }
+}
